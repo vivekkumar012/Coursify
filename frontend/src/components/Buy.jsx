ff3ff6b372ddbbab13025a45dc1891d3b0fc3427
Vivek Kumar <vh118854@gmail.com>
2025-07-10 01:22:26 +0530

    payment integration

--- frontend/src/components/Buy.jsx
+++ frontend/src/components/Buy.jsx
@@ -112,29 +112,105 @@ function Buy() {
         status: paymentIntent.status,
       };
       console.log("Payment info: ", paymentInfo);
-      await axios
-        .post(`${BACKEND_URL}/order`, paymentInfo, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-          withCredentials: true,
-        })
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-          toast.error("Error in making payment");
-        });
+      // await axios
+      //   .post(`${BACKEND_URL}/order`, paymentInfo, {
+      //     headers: {
+      //       Authorization: `Bearer ${token}`,
+      //     },
+      //     withCredentials: true,
+      //   })
+      //   .then((response) => {
+      //     console.log(response.data);
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //     toast.error("Error in making payment");
+      //   });
       toast.success("Payment Successful");
       navigate("/purchases");
     }
     setLoading(false);
   }
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <button className='bg-blue-500 rounded-md py-2 px-4 hover:bg-blue-800 text-white duration-300' onClick={handlePurchase} disabled={loading}>{loading ? "Processing...": "Buy Now"}</button>
-    </div>
+    <>
+      {error ? (
+        <div className="flex justify-center items-center h-screen">
+          <div className="bg-red-100 text-red-700 px-6 py-4 rounded-lg">
+            <p className="text-lg font-semibold">{error}</p>
+            <Link
+              className="w-full bg-orange-500 text-white py-2 rounded-md hover:bg-orange-600 transition duration-200 mt-3 flex items-center justify-center"
+              to={"/purchases"}
+            >
+              Purchases
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col sm:flex-row my-40 container mx-auto">
+          <div className="w-full md:w-1/2">
+            <h1 className="text-xl font-semibold underline">Order Details</h1>
+            <div className="flex items-center text-center space-x-2 mt-4">
+              <h2 className="text-gray-600 text-sm">Total Price</h2>
+              <p className="text-red-500 font-bold">${course.price}</p>
+            </div>
+            <div className="flex items-center text-center space-x-2">
+              <h1 className="text-gray-600 text-sm">Course name</h1>
+              <p className="text-red-500 font-bold">{course.title}</p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 flex justify-center items-center">
+            <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-sm">
+              <h2 className="text-lg font-semibold mb-4">
+                Process your Payment!
+              </h2>
+              <div className="mb-4">
+                <label
+                  className="block text-gray-700 text-sm mb-2"
+                  htmlFor="card-number"
+                >
+                  Credit/Debit Card
+                </label>
+                <form onSubmit={handlePurchase}>
+                  <CardElement
+                    options={{
+                      style: {
+                        base: {
+                          fontSize: "16px",
+                          color: "#424770",
+                          "::placeholder": {
+                            color: "#aab7c4",
+                          },
+                        },
+                        invalid: {
+                          color: "#9e2146",
+                        },
+                      },
+                    }}
+                  />
+
+                  <button
+                    type="submit"
+                    disabled={!stripe || loading} // Disable button when loading
+                    className="mt-8 w-full bg-indigo-500 text-white py-2 rounded-md hover:bg-indigo-600 transition duration-200"
+                  >
+                    {loading ? "Processing..." : "Pay"}
+                  </button>
+                </form>
+                {cardError && (
+                  <p className="text-red-500 font-semibold text-xs">
+                    {cardError}
+                  </p>
+                )}
+              </div>
+
+              <button className="w-full bg-orange-500 text-white py-2 rounded-md hover:bg-orange-600 transition duration-200 mt-3 flex items-center justify-center">
+                <span className="mr-2">🅿️</span> Other Payments Method
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
